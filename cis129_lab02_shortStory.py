f30adfa7f7f667d692493a019d89ff6b2e5a30ab
oacarbajal <oacarbajal@mail.pima.edu>
2025-02-15 10:15:22 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,4 +1,4 @@
 # Author: Omar Carbajal
 # My first Python lab in my CIS129 course
 # Print(In this course, i aim to commence my journey of gaining knowledge in programming, 
-        This is with an effort to advance my career and fulfill my life long goal.)
+        This is with an effort to advance my career and fulfill my life long goal of bebeccoming an engineer.)
